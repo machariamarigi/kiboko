c329a06457bd6f4b0f585f2a99a7ddd1662409ca
Macharia Marigi <machariamarigi@gmail.com>
2024-04-13 17:27:53 +0300

    use PRODUCTS_CATEGORIES to generate navbar items

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,38 @@
 "use client";
 
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import NavItem from "./NavItem";
+
 const NavItems = () => {
-  return <div>Nav Items Component</div>;
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+
+        const isOpen = activeIndex === index;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
