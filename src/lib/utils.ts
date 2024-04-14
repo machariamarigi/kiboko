fb6e87788442bbad267885a75c0e09416245525e
Macharia Marigi <machariamarigi@gmail.com>
2024-04-15 01:09:09 +0300

    add utilty function for formating test with currency notation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'USD' | 'EUR' | 'GBP' | 'KEN'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'KES', notation = 'compact' } = options
+
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('sw-KE', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
 }
