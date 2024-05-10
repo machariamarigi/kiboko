925ed52453a38b17f4c0290fea6eb72fa153489b
Macharia Marigi <machariamarigi@gmail.com>
2024-05-10 18:11:50 +0300

    add server

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  app.use((req, res) => nextHandler(req, res));
+  nextApp.prepare().then(() => {
+    payload.logger.info(`Next.js is ready`);
+
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js App http://localhost:${PORT}`);
+    });
+  });
+};
+
+start();
